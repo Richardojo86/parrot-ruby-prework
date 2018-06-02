6453301060d16374adfff3204429427916602ec0
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-02 04:28:06 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string = "ally wanna cracker")
+def parrot(string = "Squawk!")
   puts string
   string
 end 
