4ef6c20f3376747c72d046b592a62d4832845b34
richardojo04@gmail.com <richardojo04@gmail.com>
2018-06-02 04:27:34 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(element = "ally wanna cracker")
-  puts "Ally would like a cracker"
-  element
+def parrot(string = "ally wanna cracker")
+  puts string
+  string
 end 
